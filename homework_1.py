78ad6474442802135abaafd7c486fb6239eba480
Kucheva Oksana <makarel@ukr.net>
2022-12-05 23:09:38 +0200

    task 3, task 4, task 5

--- homework_1.py
+++ homework_1.py
@@ -1,3 +1,15 @@
+import __hello__
+# task 4
 print(' **  *\t *\t**\t   *\t*\t*\t *\n*  * *\t*  *  *   * *\t**\t*\t* *\n*  * * *   *\t  * *\t**\t*\t* *')
 print('*  * ** \t*\t *\t *\t* * *  *   *\n*  * * *\t *\t *****\t* * *  *****\n*  * *\t*  *  * *\t  * *  ** * \t*')
 print(" **  *\t *\t**\t*\t  * *  ** * \t*")
+
+# task 5
+print('Escape sequences')
+print('\\a  \tBell (alert)')
+print('\\b  \tBackspace')
+print('\\n  \tNew line')
+print('\\t  \tHorizontal tab')
+print('\\\\  \tBackslash \\')
+print('\\"  \tDouble quotation mark \"')
+print("\\'  \tSingle quotation mark \'")
