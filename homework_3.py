d65642a991317ab5d36d683709549f906fc546ea
Kucheva Oksana <makarel@ukr.net>
2022-12-17 16:49:05 +0200

    task 1, task 2

--- homework_3.py
+++ homework_3.py
@@ -14,3 +14,12 @@ elif operation == '**':
     print(f'{operand_1} {operation} {operand_2} = {operand_1 ** operand_2}')
 else:
     print('Введіть коректно операцію над числами')
+
+# task 2
+N = int(input('Введіть число: '))
+for i in range(1, N):
+    number_squared = i ** 2
+    if number_squared > N:
+        break
+    print(number_squared, end=' ')
+
