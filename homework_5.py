6026a25ea003795bc3d0277b06102e9b70368d15
Kucheva Oksana <makarel@ukr.net>
2022-12-28 13:41:03 +0200

    task 1, task 2

--- homework_5.py
+++ homework_5.py
@@ -1,9 +1,16 @@
 # task_1
 import random
 
-Li = [random.randint(1, 300) for i in range(30)]
-for element in Li:
+my_List = [random.randint(1, 300) for i in range(30)]
+for element in my_List:
     if element > 100:
         print(element, end=' ')
 print()
-print(Li)
+print('my_List', my_List)
+
+# task_2
+my_results = []
+for element in my_List:
+    if element > 100:
+        my_results.append(element)
+print('my_results', my_results)
