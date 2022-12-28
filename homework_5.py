c5291bf2b777ac962e566614d9266297deabf871
Kucheva Oksana <makarel@ukr.net>
2022-12-28 13:56:58 +0200

    task 1, task 2, task 3

--- homework_5.py
+++ homework_5.py
@@ -14,3 +14,13 @@ for element in my_List:
     if element > 100:
         my_results.append(element)
 print('my_results', my_results)
+
+# task_3
+n = int(input('Введіть число до 10: '))
+my_List_1 = [random.randint(1, 100) for i in range(n)]
+print(my_List_1)
+if len(my_List_1) < 2:
+    my_List_1.append(0)
+else:
+    my_List_1.append(my_List_1[-1] + my_List_1[-2])
+print('my_List_1', my_List_1)
