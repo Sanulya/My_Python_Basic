ab16b8f8f9f5176dd66e75080f184316fa032496
Kucheva Oksana <makarel@ukr.net>
2022-12-28 15:04:27 +0200

    task 1, task 2, task 3, task 6, task 4

--- homework_5.py
+++ homework_5.py
@@ -7,6 +7,7 @@ for element in my_List:
         print(element, end=' ')
 print()
 print('my_List', my_List)
+print()
 
 # task_2
 my_results = []
@@ -14,8 +15,9 @@ for element in my_List:
     if element > 100:
         my_results.append(element)
 print('my_results', my_results)
+print()
 
-# task_3
+print('task_3')
 n = int(input('Введіть число до 10: '))
 my_List_1 = [random.randint(1, 100) for i in range(n)]
 print(my_List_1)
@@ -24,14 +26,24 @@ if len(my_List_1) < 2:
 else:
     my_List_1.append(my_List_1[-1] + my_List_1[-2])
 print('my_List_1', my_List_1)
+print()
 
-# task_6
+print('task_6')
 List_1 = [random.randint(1, 20) for i in range(7)]
 List_2 = [random.randint(1, 20) for i in range(5)]
 result_list = List_1 + List_2
-print(result_list)
+print('result_list', result_list)
 unique_numbers = []
 for element in result_list:
     if element not in unique_numbers:
         unique_numbers.append(element)
 print('unique_numbers', unique_numbers)
+
+print('task_4')
+Li = [random.randint(1, 100) for i in range(10)]
+print('Li', Li)
+num = int(input('Введіть число до 9: '))
+Li[num::] = Li[(num + 1)::]
+Li.pop()
+print('Li', Li)
+
